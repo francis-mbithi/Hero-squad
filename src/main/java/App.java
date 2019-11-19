a821076a8480b1968d73031af2b93f2006157b6e
francis-mbithi <frankmbithifm@gmail.com>
2019-11-19 12:39:42 +0300

    add heros and squad call back functions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,21 +6,16 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class App {
-
     public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
-
-
-        get("/", (request, response) -> {
+        get("/",(request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("hero", request.session().attribute("heroes"));
-            model.put("template", "templates/index.vtl");
+            model.put("template","templates/index.vtl");
             return new ModelAndView(model, layout);
-        }, new VelocityTemplateEngine());
+        },new VelocityTemplateEngine());
 
         get("heroes/new", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
@@ -35,6 +30,19 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+
+//        post("/heroes",(request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            String name = request.queryParams("name");
+//            String age = request.queryParams("age");
+//            String power = request.queryParams("power");
+//            String weakness = request.queryParams("weakness");
+//
+//            Hero hero = new Hero(name, age, power, weakness);
+//            model.put("template", "templates/success.vtl");
+//
+//            return new ModelAndView(model, layout);
+//        },new VelocityTemplateEngine());
 
         post("/heroes", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
